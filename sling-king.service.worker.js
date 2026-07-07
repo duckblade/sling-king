15ab135785431e6d5b113b7538c89e8d83a18248
duckblade <duckblade0@gmail.com>
2026-07-07 23:00:40 +0200

    mobile v

--- sling-king.service.worker.js
+++ sling-king.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783336890|510338540';
+const CACHE_VERSION = '1783458003|117262496';
 /** @type {string} */
 const CACHE_PREFIX = 'platformer-tutor-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
