193005aa391f1a1abf4de55c855110778a70c818
duckblade <duckblade0@gmail.com>
2026-08-02 23:17:07 +0200

    vO.62

--- sling-king.service.worker.js
+++ sling-king.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785595037|8540543891';
+const CACHE_VERSION = '1785705408|30817974422';
 /** @type {string} */
 const CACHE_PREFIX = 'sling-king-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
